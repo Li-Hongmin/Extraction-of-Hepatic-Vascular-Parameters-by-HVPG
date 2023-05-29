5ab32140d68af2319b78e565d81c51258b0d33b7
Fangyanping <lihongmin@alumni.u-tokyo.ac.jp>
2023-05-29 19:25:29 +0800

    fix bugs

--- fixbug_vmtk.py
+++ fixbug_vmtk.py
@@ -5,7 +5,7 @@ import vmtk
 # find vmtkcenterlines.py in vmtk package
 
 vmtkcenterlines_path = os.path.join(os.path.dirname(vmtk.__file__), "vmtkcenterlines.py")
-
+print(vmtkcenterlines_path)
 #%%
 
 # Read the contents of the vmtkcenterlines.py file
@@ -13,6 +13,7 @@ with open(vmtkcenterlines_path, 'r') as f:
     contents = f.read()
 
 # Replace len(self.SourcePoints)/3 with int(len(self.SourcePoints)/3)
+new_contents = contents.replace("len(self.TargetPoints)/3", "int(len(self.TargetPoints)/3)")
 new_contents = contents.replace("len(self.SourcePoints)/3", "int(len(self.SourcePoints)/3)")
 
 # Write the modified contents back to the vmtkcenterlines.py file
